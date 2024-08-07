fdb8932c37c8f3cbce1d70feab4c1d6f4f0cf88e
Andrea Ottaviani <andrea.ottaviani.69@gmail.com>
2024-08-07 10:12:08 +0200

    Update inty_cart.c
    bug fixed in hacks check

--- Source/VSC-PiRTOII/inty_cart.c
+++ Source/VSC-PiRTOII/inty_cart.c
@@ -335,8 +335,8 @@ while(1) {
             if (parallelBus==HACK[i]) {
               dataOut=HACK_CODE[i];
               deviceAddress = true;
+	      break;
             }
-            break;
           }
         } 
       }
